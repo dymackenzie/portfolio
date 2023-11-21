6dec27974a467351b050d1439719cffb2b33d791
kenzie <kenziedy@students.cs.ubc.ca>
2023-11-21 15:57:21 -0800

    finished experiences part

--- personal_project/js/project_scrolling.js
+++ personal_project/js/project_scrolling.js
@@ -28,6 +28,18 @@ gsap.from("#decor-box2", {
     }
 })
 
+gsap.from("#project-title", {
+    xPercent: 50,
+    scrollTrigger: {
+        trigger: '.main',
+        start: 'bottom bottom',
+        end: 'bottom top',
+        ease: 'circ',
+        scrub: true,
+        toggleActions: 'play pause reverse reverse'
+    }
+})
+
 gsap.to(".project-container", {
     xPercent: -78,
     scrollTrigger: {
